7155343fde440db0446caeae6d60580d518b5f3d
Sampath Varma Datla <sampathvarma349@gmail.com>
2025-05-23 11:25:28 +0530

    Updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -78,7 +78,7 @@ const Footer = () => {
           {/* Contact Info */}
           <div className="col-span-1">
             <h4 className="text-[#2D1247] font-medium mb-4">Contact Info</h4>
-            <div className="space-y-3 text-sm">
+            <div className="space-y-4 text-sm">
               <p className="flex items-start text-[#2D1247] font-bold">
                 <svg className="w-6 h-6 mr-2.5 mt-0.5 text-purple-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path>
